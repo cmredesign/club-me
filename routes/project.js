69ea527a6eb18737c0803a000bcd50a671173ffb
hungale <zenithparadox@gmail.com>
2016-02-10 01:49:54 +0000

    fixed back btn

--- routes/project.js
+++ routes/project.js
@@ -1,5 +1,5 @@
-exports.viewProject = function(req, res) { 
-   	var name = req.params.name; 
+exports.viewProject = function(req, res) {
+   	var name = req.params.name;
   	console.log("The project name is: " + name);
    	res.render('project', {
    		 'projectName': name
