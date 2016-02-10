b3008340a36d274901a78921b7dd74acb06816f2
vagrant <vagrant@precise32.(none)>
2016-02-10 02:23:58 +0000

    sorry

---
+++ routes/update.js
@@ -0,0 +1,23 @@
+/*
+ * GET settings god damn it
+*/
+
+exports.view = function(req, res){
+  console.log("first one");
+  /*res.render('settings', {
+    'settings': [
+    { 'name': 'Email Notifications',
+      'image': 'Checked.png',
+      'id': 'setting1'
+    },
+    { 'name': 'Dark Mode',
+      'image': 'Checked.png',
+      'id': 'setting2'
+    },
+    { 'name': 'Notify Clubs When I Bookmark Them',
+      'image': 'Checked.png',
+      'id': 'setting3'
+    },
+    ]
+  });*/
+}
