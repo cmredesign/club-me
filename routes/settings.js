fab13c74ac6ac74f4571c6ac5fa2cdcace986bd2
vagrant <vagrant@precise32.(none)>
2016-02-11 02:12:29 +0000

    Added more settings, text/image on same line. Some visual issues.

--- routes/settings.js
+++ routes/settings.js
@@ -5,17 +5,29 @@
 exports.view = function(req, res){
   res.render('settings', {
     'settings': [
-    { 'name': 'Email Notifications',
+    { 'name': 'Message Notifications',
       'image': 'Checked.png',
       'id': 'setting1'
     },
-    { 'name': 'Dark Mode',
+    { 'name': 'Bookmark Notifications',
       'image': 'Checked.png',
       'id': 'setting2'
     },
-    { 'name': 'Notify Clubs When I Bookmark Them',
+    { 'name': 'Notify Clubs I Bookmark',
       'image': 'Checked.png',
       'id': 'setting3'
+    },
+    { 'name': 'Allow Clubs to Find Me',
+      'image': 'Checked.png',
+      'id': 'setting4'
+    },
+    { 'name': 'Allow Clubs to Message Me First',
+      'image': 'Checked.png',
+      'id': 'setting5'
+    },
+    { 'name': 'Disable ALL Notifications',
+      'image': 'Checked.png',
+      'id': 'setting6'
     },
     ]
   });
