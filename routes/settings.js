ff91e112b6fa0ae93c1b2113f1e1b82fcad627a8
vagrant <vagrant@precise32.(none)>
2016-02-11 02:49:53 +0000

    Added foundation of toggling checkmarks. Not functional, unsure why.

--- routes/settings.js
+++ routes/settings.js
@@ -1,34 +1,58 @@
 /*
- * GET settings god damn it
+ * GET settings
 */
 
 exports.view = function(req, res){
   res.render('settings', {
     'settings': [
     { 'name': 'Message Notifications',
-      'image': 'Checked.png',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
       'id': 'setting1'
     },
     { 'name': 'Bookmark Notifications',
-      'image': 'Checked.png',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
       'id': 'setting2'
     },
     { 'name': 'Notify Clubs I Bookmark',
-      'image': 'Checked.png',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
       'id': 'setting3'
     },
     { 'name': 'Allow Clubs to Find Me',
-      'image': 'Checked.png',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
       'id': 'setting4'
     },
     { 'name': 'Allow Clubs to Message Me First',
-      'image': 'Checked.png',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
       'id': 'setting5'
     },
     { 'name': 'Disable ALL Notifications',
-      'image': 'Checked.png',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
       'id': 'setting6'
     },
     ]
   });
+}
+
+function toggleCheck(id) 
+{
+    var theImg = document.getElementById(id),
+             x = theImg.src.split("/"),
+             t = x.length-1,
+             y = x[t];
+
+    if(y == 'Checked.png')
+    {
+        theImg.src='./images/Unchecked.jpg'
+    }
+
+    if(y == 'Unchecked.jpg')
+    {
+        theImg.src='./images/Checked.png'
+    }
 }
