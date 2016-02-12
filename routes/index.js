ed146b4ff7596d456e6468b99776cedc7ccd02c0
hungale <zenithparadox@gmail.com>
2016-02-12 05:32:02 +0000

    settings, signup, update fixed

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,3 @@ exports.view = function(req, res){
   });
 }
 
-
--- routes/settings.js
+++ routes/settings.js
@@ -1,18 +1,35 @@
-
 exports.view = function(req, res){
   res.render('settings', {
     'settings': [
-    { 'name': 'Email Notifications',
-      'image': 'Checked.png',
+    { 'name': 'Message Notifications',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
       'id': 'setting1'
     },
-    { 'name': 'Dark Mode',
-      'image': 'Checked.png',
+    { 'name': 'Bookmark Notifications',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
       'id': 'setting2'
     },
-    { 'name': 'Notify Clubs When I Bookmark Them',
-      'image': 'Checked.png',
+    { 'name': 'Notify Clubs I Bookmark',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
       'id': 'setting3'
+    },
+    { 'name': 'Allow Clubs to Find Me',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
+      'id': 'setting4'
+    },
+    { 'name': 'Allow Clubs to Message Me First',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
+      'id': 'setting5'
+    },
+    { 'name': 'Disable ALL Notifications',
+      'image1': 'Checked.png',
+      'image2' : 'Unchecked.jpg',
+      'id': 'setting6'
     },
     ]
   });
