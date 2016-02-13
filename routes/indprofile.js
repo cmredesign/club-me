4378b3afd9933a12fc9ad92d4a29c8fe847fd688
vagrant <vagrant@precise32.(none)>
2016-02-13 23:41:46 +0000

    homepage

---
+++ routes/indprofile.js
@@ -0,0 +1,28 @@
+
+/*
+ * GET home page.
+ */
+
+exports.view = function(req, res){
+   res.render('indprofile', {
+    'projects': [
+      { 'name': 'Match Me',
+        'image': 'match.png',
+        'id': 'project1'
+      },
+      { 'name': 'Search',
+        'image': 'search.png',
+        'id': 'project2'
+      },
+      { 'name': 'My Clubs',
+        'image': 'mine.png',
+        'id': 'project3'
+      },
+      { 'name': 'Update Profile',
+        'image': 'prof.png',
+        'id': 'project4'
+      },
+    ]
+  });
+}
+
