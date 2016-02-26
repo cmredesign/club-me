95833ce6ac8c5a48283ff88f4ac84ee1cfd22756
hungale <zenithparadox@gmail.com>
2016-02-26 04:03:14 +0000

    test search bar

--- public/js/searchbar_old.js
+++ public/js/searchbar_old.js
@@ -2,16 +2,34 @@
 
 $(document).ready(function () {
 
-  var sbar = 
-    '<div id="bottomnav">'+
-      '<div id=>'+
-      '<ul class>'+
-        '<li><a href="/indprofile">Profile</a></li>'+
-        '<li class="/active"><a href="#">Match Me</a></li>'+
-        '<li><a href="/myclubs">My Clubs</a></li>'+
-        '<li><a href="/search">Club Search</a></li>'+
-        '<li><a href="/settings">Help</a></li>'+
-      '</ul>'+
+  var sbar = '<div class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="slide-nav">'+
+    '<div class="container">'+
+      '<div class="navbar-header">'+
+        '<a class="navbar-toggle">'+
+          '<span class="sr-only">Toggle navigation</span>'+
+          '<span class="icon-bar"></span>'+
+          '<span class="icon-bar"></span>'+
+          '<span class="icon-bar"></span>'+
+        '</a>'+
+        '<a class="col-m-6" id="imaginary_container" href="#">'+
+          '<div class="input-group stylish-input-group">'+
+            '<input type="text" class="form-control"  placeholder="Search" >'+
+            '<span class="input-group-addon">'+
+              '<button type="submit">'+
+                '<span class="glyphicon glyphicon-search"></span>'+
+              '</button>'+
+            '</span>'+
+          '</div>'+
+        '</a>'+
+      '</div>'+
+      '<div id="slidemenu">'+
+        '<ul class="nav navbar-nav">'+
+          '<li><a href="/indprofile">Profile</a></li>'+
+          '<li class="/active"><a href="#">Match Me</a></li>'+
+          '<li><a href="/myclubs">My Clubs</a></li>'+
+          '<li><a href="/search">Club Search</a></li>'+
+          '<li><a href="/settings">Help</a></li>'+
+        '</div>'+
       '</div>'+
     '</div>';
 
